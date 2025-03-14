f93ad48945a07b6d9650f31b1c00f6afe5d203c8
emilndersen <emilnsinnk@gmail.com>
2025-03-14 12:14:23 +0400

    Initial commit

---
+++ src/test/java/com/lexspace/LexSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
