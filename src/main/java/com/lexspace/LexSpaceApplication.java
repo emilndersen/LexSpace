3bdf303963ba9e01fece2e2d8f8cdf03f6f18172
emilndersen <emilnsinnk@gmail.com>
2025-03-14 19:36:41 +0400

    Removed the restriction on /src/

---
+++ src/main/java/com/lexspace/LexSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.lexspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexSpaceApplication.class, args);
+	}
+
+}
